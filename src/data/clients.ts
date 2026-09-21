9b298d03971ff0ec1afc19c7f731fd36d589cb44
smartlinkbox2025-droid <smartlinkbox2025@gmail.com>
2026-09-22 02:22:12 +0300

    Add source file src/data/clients.ts

---
+++ src/data/clients.ts
@@ -0,0 +1,12 @@
+export const clients = [
+  { ar: "شركة بنمو العربية للمقاولات", en: "Arabian BEMCO Contracting Co. Ltd." },
+  { ar: "الفنار", en: "alfanar" },
+  { ar: "نسما وشركاه", en: "Nesma & Partners" },
+  { ar: "زامل للحديد", en: "Zamil Steel" },
+  { ar: "ABB", en: "ABB" },
+  { ar: "CGS", en: "CGS — Automotive Refrigeration Special Products" },
+  { ar: "شركة العرب للمقاولات", en: "Al Arrab Contracting Co." },
+  { ar: "الهيئة العربية السعودية للمواصفات والمقاييس", en: "Saudi Standards and Specifications authority" },
+  { ar: "مجموعة بن لادن السعودية", en: "Saudi Binladin Group" },
+  { ar: "شركة ابتكار المعمار للتجارة والمقاولات", en: "Ebtekar Almemar Trading & Contracting Co." },
+];
